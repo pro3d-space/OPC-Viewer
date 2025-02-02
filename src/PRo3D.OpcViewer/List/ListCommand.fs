e40903a318b4d38ccfa8dff05f0184c44b51b01c
Stefan Maierhofer <sm@stefanmaierhofer.com>
2025-02-02 10:48:11 +0100

    commands list, view, diff (wip)

--- src/PRo3D.OpcViewer/List/ListCommand.fs
+++ src/PRo3D.OpcViewer/List/ListCommand.fs
@@ -7,12 +7,13 @@ module ListCommand =
 
     type Args =
         | [<MainCommand>] DataDirs of data_dir: string list
+        | [<Unique;AltCommandLine("-d") >] Detail
 
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
                 | DataDirs _ -> "specify data directories"
-
+                | Detail     -> "show detail info"
 
     let run (args : ParseResults<Args>) : int =
 
@@ -24,10 +25,29 @@ module ListCommand =
                 exit 0
                 []
 
-        // discover all layers in datadirs ...
-        let layerInfos = LayerManagement.searchLayerDirs datadirs
+        let showDetails = args.Contains(Args.Detail)
 
-        // print ...
-        for x in layerInfos do printfn "[layer] %s" x.Path.FullName
+        // discover all layers in datadirs ...
+        let layerInfos =
+            LayerUtils.searchLayerDirs datadirs
+            |> List.sortBy (fun x -> x.Path.FullName)
+
+        for info in layerInfos do
+            printfn "%s" info.Path.FullName
+
+            if showDetails then
+                let f (i : int) = i.ToString("N0")
+
+                let stats = info |> LayerUtils.loadPatchHierarchy |> LayerUtils.getPatchHierarchyStats
+                let countNodes = stats.CountInnerNodes + stats.CountLeafNodes
+                if stats.IndexedAttributes.Length > 0 then
+                    let s = String.concat ", " (stats.IndexedAttributes |> Array.map(fun x -> x.ToString()))
+                    printfn "    %s" s
+                if stats.SingleAttributes.Length > 0 then
+                    let s = String.concat ", " (stats.SingleAttributes |> Array.map(fun x -> x.ToString()))
+                    printfn "    %s" s
+
+                printfn "    %12s nodes (%s leafs, %s inner)" (f(countNodes)) (f(stats.CountLeafNodes)) (f(stats.CountInnerNodes))
+                printfn "    %12s vertices %12s faces" (f(stats.CountVertices)) (f(stats.CountFaces))
 
         0
--- src/PRo3D.OpcViewer/Usage.fs
+++ src/PRo3D.OpcViewer/Usage.fs
@@ -7,13 +7,15 @@ module Usage =
 
     type CliArguments =
         | Version
-        | [<CliPrefix(CliPrefix.None); AltCommandLine("ls")>] List of ParseResults<ListCommand.Args>
-        | [<CliPrefix(CliPrefix.None)>] View of ParseResults<ViewCommand.Args>
+        | [<CliPrefix(CliPrefix.None)                       >] Diff of ParseResults<DiffCommand.Args>
+        | [<CliPrefix(CliPrefix.None); AltCommandLine("ls") >] List of ParseResults<ListCommand.Args>
+        | [<CliPrefix(CliPrefix.None)                       >] View of ParseResults<ViewCommand.Args>
 
         interface IArgParserTemplate with
             member s.Usage =
                 match s with
                 | Version -> "Print program version."
+                | Diff _ -> "Compute difference between a layer with other layers."
                 | List _ -> "List datasets."
                 | View _ -> "View datasets."
 
