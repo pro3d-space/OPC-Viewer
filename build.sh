7dce3e38e1fdc559b9c1bdd1f0df5b43ef9b2cda
Stefan Maierhofer <--global>
2025-03-20 04:57:30 +0100

    fix build.sh and opcviewer

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 dotnet tool restore
 dotnet paket restore
 #dotnet adaptify --local --verbose --force .\src\PRo3D.WebViewer\PRo3D.OpcViewer.fsproj
-dotnet build
+dotnet build -c Release
 
 #dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained -o publish/PRo3D.OpcViewer src/PRo3D.OpcViewer
