d8d1885298eae0e2079a976cb787a07911a2352a
Stefan Maierhofer <sm@stefanmaierhofer.com>
2025-05-28 10:49:34 +0200

    sync

--- src/PRo3D.OpcViewer/Program.fs
+++ src/PRo3D.OpcViewer/Program.fs
@@ -1,10 +1,28 @@
 ﻿open Argu
 open PRo3D.OpcViewer
-open PRo3D.OpcViewer.Data
 open System.IO
 
 [<EntryPoint>]
 let main argv =
+
+    //do
+    //    let json = File.ReadAllText(@"W:\Datasets\Pro3D\confidential\Vaughan-Boulders-2025\Vaughan-Boulders-2025.pro3d.json")
+    //    let pro3d = Pro3DFile.parse json
+
+    //    let skipPrefix = @"D:\M20\"
+    //    let xs =
+    //        pro3d.SurfaceModel.Surfaces.Flat
+    //        |> Array.collect (fun x -> x.Surfaces.OpcPaths)
+    //        |> Array.map (fun x ->
+    //            if not (x.StartsWith(skipPrefix)) then printfn "  [ERROR] path %s does not start with %s" x skipPrefix
+    //            x.Replace('\\', '/').Substring(skipPrefix.Length)
+    //            )
+    //        |> Array.sort
+        
+    //    for x in xs do
+    //        printfn "%s" x
+
+    //    System.Environment.Exit(0)
 
     let PROGRAM_NAME = "PRo3D.OpcViewer"
     let VERSION      = "1.0.1"
